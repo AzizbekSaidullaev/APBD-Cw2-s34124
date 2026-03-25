167ccbc05f649c685cc1a6ec0653909f4ffe717d
Azizbek Saidullaev <s34124@pjwstk.edu.pl>
2026-03-25 21:30:51 +0100

    dodalem wypozyczenie.

---
+++ rental.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RentalSystem.Domain
+{
+    public class Rental
+    {
+        public User RentedBy { get; }
+        public Equipment RentedEquipment { get; }
+        public DateTime RentDate { get; }
+        public DateTime DueDate { get; }
+        public DateTime? ActualReturnDate { get; private set; }
+        public decimal Penalty { get; private set; } = 0;
+
+        public Rental(User user, Equipment equipment, DateTime rentDate, TimeSpan duration)
+        {
+            RentedBy = user;
+            RentedEquipment = equipment;
+            RentDate = rentDate;
+            DueDate = rentDate.Add(duration);
+        }
+
+        public void MarkAsReturned(DateTime returnDate, decimal penaltyAmount)
+        {
+            ActualReturnDate = returnDate;
+            Penalty = penaltyAmount;
+        }
+    }
+}
