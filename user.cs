a87ded441c65feb7b0df18648c4ae2677146d2b6
Azizbek Saidullaev <s34124@pjwstk.edu.pl>
2026-03-25 21:21:02 +0100

    dodalem model usera.

---
+++ user.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RentalSystem.Domain
+{
+    public abstract class User
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public string FirstName { get; }
+        public string LastName { get; }
+        public abstract int MaxActiveRentals { get; }
+
+        protected User(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+
+    public class Student : User
+    {
+        public override int MaxActiveRentals => 2;
+        public Student(string firstName, string lastName) : base(firstName, lastName) { }
+    }
+
+    public class Employee : User
+    {
+        public override int MaxActiveRentals => 5;
+        public Employee(string firstName, string lastName) : base(firstName, lastName) { }
+    }
+}
