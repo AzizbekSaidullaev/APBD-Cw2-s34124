3d4f89d2cb202bb79a031867df62b5053d96922c
Azizbek Saidullaev <s34124@pjwstk.edu.pl>
2026-03-25 21:33:36 +0100

    dodalem szkielet serwisu wypoz.

---
+++ rentalservice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RentalSystem.Domain;
+
+namespace RentalSystem.Services
+{
+    public class RentalService
+    {
+    
+    }
+}
